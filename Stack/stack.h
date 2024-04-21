af96dab03a2694424a24513afea5ef8657ef5047
TarcisMoreda <tarcismoreda@gmail.com>
2024-04-21 11:34:18 -0300

    Update consts

---
+++ Stack/stack.h
@@ -0,0 +1,13 @@
+#if !defined(STACK_H)
+#define STACK_H
+
+typedef struct stack_t Stack;
+
+Stack *stack_create();
+unsigned int stack_size(Stack *stack);
+char stack_push(Stack *stack, const int value);
+int stack_pop(Stack *stack);
+char stack_clear(Stack *stack);
+void stack_destroy(Stack *stack);
+
+#endif // STACK_H
